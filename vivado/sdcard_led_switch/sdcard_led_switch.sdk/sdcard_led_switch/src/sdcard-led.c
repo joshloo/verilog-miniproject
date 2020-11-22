eea7743b3e0d9116654cd3b8975dbfe3e94890a6
Loo, Tung Lun <joshlootunglun@gmail.com>
2020-11-22 16:51:40 +0800

    add function headers

--- vivado/sdcard_led_switch/sdcard_led_switch.sdk/sdcard_led_switch/src/sdcard-led.c
+++ vivado/sdcard_led_switch/sdcard_led_switch.sdk/sdcard_led_switch/src/sdcard-led.c
@@ -35,7 +35,10 @@
 /**************************** Type Definitions *******************************/
 /***************** Macros (Inline Functions) Definitions *********************/
 /************************** Function Prototypes ******************************/
-int FfsSdPolledExample(void);
+int writeFile1(void);
+int writeFile2(void);
+int writeFile3(void);
+int readFile(int fileNum);
 
 /************************** Variable Definitions *****************************/
 static FIL fil;		/* File object */
@@ -447,92 +450,6 @@ int writeFile3(void){
 	/* Data verification */
 	for(BuffCnt = 0; BuffCnt < FileSize; BuffCnt++){
 		xil_printf("content written : %d \r\n",DestinationAddress[BuffCnt]);
-		if(SourceAddress[BuffCnt] != DestinationAddress[BuffCnt]){
-			return XST_FAILURE;
-		}
-	}
-	Res = f_close(&fil);
-	if (Res) {
-		return XST_FAILURE;
-	}
-	return XST_SUCCESS;
-}
-
-
-int FfsSdPolledExample(void)
-{
-	FRESULT Res;
-	UINT NumBytesRead;
-	UINT NumBytesWritten;
-	u32 BuffCnt;
-	u32 FileSize = 1;
-	/*
-	 * To test logical drive 0, Path should be "0:/"
-	 * For logical drive 1, Path should be "1:/"
-	 */
-	TCHAR *Path = "0:/";
-
-	Platform = XGetPlatform_Info();
-
-	for(BuffCnt = 0; BuffCnt < FileSize; BuffCnt++){
-		SourceAddress[BuffCnt] = 3;
-	}
-
-	/* Register volume work area, initialize device	 */
-	Res = f_mount(&fatfs, Path, 0);
-
-	if (Res != FR_OK) {
-		xil_printf("SD f_mount failed \r\n");
-		return XST_FAILURE;
-	}
-
-/*	Res = f_mkfs(Path, 0, 0);
-	if (Res != FR_OK) {
-		xil_printf("SD f_mkfs failed \r\n");
-		return XST_FAILURE;
-	}
-*/
-	SD_File = (char *)FileName;
-
-	Res = f_open(&fil, SD_File, FA_CREATE_ALWAYS | FA_WRITE | FA_READ);
-	if (Res) {
-		xil_printf("SD f_open failed \r\n");
-		return XST_FAILURE;
-	}
-
-	/* Pointer to beginning of file .	 */
-	Res = f_lseek(&fil, 0);
-	if (Res) {
-		xil_printf("SD f_lseek failed \r\n");
-		return XST_FAILURE;
-	}
-
-	/* Write data to file.
-	Res = f_write(&fil, (const void*)SourceAddress, FileSize,
-			&NumBytesWritten);
-	if (Res) {
-		xil_printf("SD f_write failed \r\n");
-		return XST_FAILURE;
-	}*/
-
-	/* Pointer to beginning of file .*/
-	Res = f_lseek(&fil, 0);
-	if (Res) {
-		return XST_FAILURE;
-	}
-
-	/* Read data from file.	 */
-	Res = f_read(&fil, (void*)DestinationAddress, FileSize,
-			&NumBytesRead);
-	if (Res) {
-		return XST_FAILURE;
-	} else {
-		xil_printf("read successful!! number of byte read is : %d \r\n", NumBytesRead);
-	}
-
-	/* Data verification */
-	for(BuffCnt = 0; BuffCnt < FileSize; BuffCnt++){
-		xil_printf("destination : %d \r\n",DestinationAddress[BuffCnt]);
 		if(SourceAddress[BuffCnt] != DestinationAddress[BuffCnt]){
 			return XST_FAILURE;
 		}
