15ac026205abfff703927ca06d69c27aa897e2c9
Loo, Tung Lun <joshlootunglun@gmail.com>
2020-11-22 16:42:13 +0800

    add some code to handle requests

--- vivado/sdcard_led_switch/sdcard_led_switch.sdk/sdcard_led_switch/src/sdcard-led.c
+++ vivado/sdcard_led_switch/sdcard_led_switch.sdk/sdcard_led_switch/src/sdcard-led.c
@@ -70,8 +70,11 @@ XGpio Gpio0, Gpio1;									/* GPIO Device driver instance */
 
 int main(void)
 {
+	xil_printf("===================\r\n");
+	xil_printf("Program starts here.\r\n");
 	int Status;
 	int sws = 0;
+	int request1 = 0, request2 = 0, request3 = 0, request = 0;
 
 	// this 3 functions write file 1 to 3.
 	// file1.bin now contains 8
@@ -109,25 +112,29 @@ int main(void)
 			sws = XGpio_DiscreteRead(&Gpio1, SWS_CHANNEL);
 			xil_printf("sws 1 data read is %d \r\n", sws);
 			XGpio_DiscreteWrite(&Gpio0, LED_CHANNEL, sws); // directly write sws settings to led
+
+			// extract out the request
+			request1 = (sws & (1 << 2)) >> 2;
+			request2 = (sws & (1 << 1)) >> 1;
+			request3 = (sws & (1 << 0)) >> 0;
+			xil_printf("request1: %d \r\n", request1);
+			xil_printf("request2: %d \r\n", request2);
+			xil_printf("request3: %d \r\n", request3);
+
+			// state machine needs to be implemented here
+			if (request1 == 1) {
+				readFile(1);
+			}
+			if (request2 == 1) {
+				readFile(2);
+			}
+			if (request3 == 1) {
+				readFile(3);
+			}
 		}
 	}
 
-
-	xil_printf("SD Polled File System Example Test \r\n");
-
-	// Before this, we already write 3 files.
-	// file1.bin consist of number 8 as the content
-	// file2.bin consist of number 5 as the content
-	// file3.bin consist of number 3 as the content
-
-	// below code will demonstrate that we are able to read from file 1 to 3.
-	Status = FfsSdPolledExample();
-	if (Status != XST_SUCCESS) {
-		xil_printf("SD Polled File System Example Test failed \r\n");
-		return XST_FAILURE;
-	}
-
-	xil_printf("Successfully ran SD Polled File System Example Test \r\n");
+	xil_printf("Program ends here.\r\n");
 	return XST_SUCCESS;
 }
 
@@ -199,6 +206,90 @@ int writeFile1(void){
 	for(BuffCnt = 0; BuffCnt < FileSize; BuffCnt++){
 		xil_printf("content written : %d \r\n",DestinationAddress[BuffCnt]);
 		if(SourceAddress[BuffCnt] != DestinationAddress[BuffCnt]){
+			return XST_FAILURE;
+		}
+	}
+	Res = f_close(&fil);
+	if (Res) {
+		return XST_FAILURE;
+	}
+	return XST_SUCCESS;
+}
+
+int readFile(int fileNum) {
+	switch (fileNum) {
+		case 1:
+			FileName[32] = "file1.bin";
+			break;
+		case 2:
+			FileName[32] = "file2.bin";
+			break;
+		case 3:
+			FileName[32] = "file3.bin";
+			break;
+		default:
+			FileName[32] = "";
+	}
+	FRESULT Res;
+	UINT NumBytesRead;
+	UINT NumBytesWritten;
+	u32 BuffCnt;
+	u32 FileSize = 1;
+	/*
+	 * To test logical drive 0, Path should be "0:/"
+	 * For logical drive 1, Path should be "1:/"
+	 */
+	TCHAR *Path = "0:/";
+
+	Platform = XGetPlatform_Info();
+
+	for (BuffCnt = 0; BuffCnt < FileSize; BuffCnt++) {
+		SourceAddress[BuffCnt] = 8;
+	}
+
+	/* Register volume work area, initialize device	 */
+	Res = f_mount(&fatfs, Path, 0);
+
+	if (Res != FR_OK) {
+		xil_printf("SD f_mount failed \r\n");
+		return XST_FAILURE;
+	}
+
+	SD_File = (char *)FileName;
+
+	Res = f_open(&fil, SD_File, FA_CREATE_ALWAYS | FA_WRITE | FA_READ);
+	if (Res) {
+		xil_printf("SD f_open failed \r\n");
+		return XST_FAILURE;
+	}
+
+	/* Pointer to beginning of file .	 */
+	Res = f_lseek(&fil, 0);
+	if (Res) {
+		xil_printf("SD f_lseek failed \r\n");
+		return XST_FAILURE;
+	}
+
+	/* Pointer to beginning of file .*/
+	Res = f_lseek(&fil, 0);
+	if (Res) {
+		return XST_FAILURE;
+	}
+
+	/* Read data from file.	 */
+	Res = f_read(&fil, (void*)DestinationAddress, FileSize,
+		&NumBytesRead);
+	if (Res) {
+		return XST_FAILURE;
+	}
+	else {
+		xil_printf("Based on request, content read : \r\n");
+	}
+
+	/* Data verification */
+	for (BuffCnt = 0; BuffCnt < FileSize; BuffCnt++) {
+		xil_printf("%d \r\n", DestinationAddress[BuffCnt]);
+		if (SourceAddress[BuffCnt] != DestinationAddress[BuffCnt]) {
 			return XST_FAILURE;
 		}
 	}
